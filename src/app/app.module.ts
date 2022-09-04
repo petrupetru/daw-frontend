76477be4969da23d703d2de851397a0260795197
Petru Burdusa <petru20019@gmail.com>
2022-09-04 16:44:15 +0300

    custom highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { JwtModule } from '@auth0/angular-jwt';
+import { HighlightDirective } from './highlight.directive';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { JwtModule } from '@auth0/angular-jwt';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
+    HighlightDirective,
     
   ],
   imports: [
